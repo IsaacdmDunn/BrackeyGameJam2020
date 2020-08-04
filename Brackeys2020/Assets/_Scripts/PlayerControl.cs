bf03c74d73cb3109f63ad39cb476de7ec3cfd486
IsaacDunn <55945831+IsaacDunn@users.noreply.github.com>
2020-08-04 18:18:40 +0100

    V0.3 Player Movement

--- Brackeys2020/Assets/_Scripts/PlayerControl.cs
+++ Brackeys2020/Assets/_Scripts/PlayerControl.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 3f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public Animator animator;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //input
+        movement.x = (int)Input.GetAxisRaw("Horizontal");
+        movement.y = (int)Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+    
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
